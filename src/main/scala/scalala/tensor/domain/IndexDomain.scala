62e15cf93a84efd5e486d36a76b2018591affb7d
Jason Zaugg <jzaugg@gmail.com>
2012-06-06 22:09:14 +0200

    The type parameter to toIndexedSeq is gone.

--- src/main/scala/scalala/tensor/domain/IndexDomain.scala
+++ src/main/scala/scalala/tensor/domain/IndexDomain.scala
@@ -41,7 +41,7 @@ extends Domain1[Int] with Domain1Like[Int,IndexDomain] {
     case _ => super.product[B,That](that);
   }
 
-  override def toIndexedSeq[B>:Int] =
+  override def toIndexedSeq =
     Range(0,size);
     
   override def contains(key : Int) =
